f0f616590c8da94153996233a296e493b24e98ee
Daniel <chesterdany22@yahoo.com>
2020-05-17 00:51:38 +0300

    contact page added and cv

--- src/UI/ContactPage/ContactPage.js
+++ src/UI/ContactPage/ContactPage.js
@@ -1,8 +1,16 @@
 import React from "react";
 import Navbar from "../NavBar/Navbar";
+import Cv from "../../assests/cv/CV-BrendeaDaniel.pdf";
 
 const contactPage = () => {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <a href={Cv} target="_blank" rel="noopener noreferrer">
+        <button>View CV</button>
+      </a>
+    </div>
+  );
 };
 
 export default contactPage;
