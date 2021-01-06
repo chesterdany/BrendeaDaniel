6a0c6aae894833f5171f2835956726671127c302
chesterdany <chesterdany22@yahoo.com>
2021-01-06 13:55:04 +0200

    ui remodel completly added

--- src/UI/Footer/Footer.js
+++ src/UI/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Footer.module.css";
 
 const footer = () => {
-  return <footer className={classes.Footer}>&copy; Copyright 2020</footer>;
+  return <footer className={classes.Footer}>&copy; Copyright 2019</footer>;
 };
 
 export default footer;
