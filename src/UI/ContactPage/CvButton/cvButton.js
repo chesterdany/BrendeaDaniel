2b246efdae2d313c4ffbe350c197e52dd20950e6
chesterdany <chesterdany22@yahoo.com>
2021-02-02 17:05:10 +0200

    redesign project page

--- src/UI/ContactPage/CvButton/cvButton.js
+++ src/UI/ContactPage/CvButton/cvButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import cv from "../../../assests/cv/CV-EN-Brendea-Daniel2.pdf";
 import classes from "./cvButton.module.css";
 
-const cvButton = (props) => {
+const cvButton = () => {
   return (
     <div className={classes.cvdivlink}>
       <a href={cv} target="_blank" rel="noopener noreferrer">
--- src/UI/ProjectPage/Project.js
+++ src/UI/ProjectPage/Project.js
@@ -1,20 +1,34 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
+import { TiEye } from "react-icons/ti";
 import classes from "./Project.module.css";
 
 const project = (props) => {
   return (
-    <div className={classes.ImageDiv}>
-      <a href={props.apphref} target="_blank" rel="noopener noreferrer">
-        <img src={props.src} alt={props.src} />
-        {/* <span className={classes.tooltip}>Try it!</span> */}
-      </a>
-      <a href={props.sourcehref} target="_blank" rel="noopener noreferrer">
-        <div className={classes.gitdiv}>
-          <FaGithub className={classes.github} />
-          <button className={classes.btn}>View Source Code</button>
-        </div>
-      </a>
+    <div className={classes.project}>
+      <div className={classes.ImageDiv}>
+        <a href={props.apphref} target="_blank" rel="noopener noreferrer">
+          <img src={props.src} alt={props.src} />
+        </a>
+      </div>
+      <div className={classes.projectBtns}>
+        <a
+          href={props.apphref}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={classes.btnLight}
+        >
+          <TiEye className={classes.github} /> Project
+        </a>
+        <a
+          href={props.sourcehref}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={classes.btnBlack}
+        >
+          <FaGithub className={classes.github} /> Github
+        </a>
+      </div>
     </div>
   );
 };
