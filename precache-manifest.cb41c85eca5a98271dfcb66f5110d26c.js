2ebaf89cba0edafc9c659e83ac686a76bb304066
Brendea Daniel <chesterdany22@yahoo.com>
2021-03-03 14:22:35 +0200

    Updates

---
+++ precache-manifest.cb41c85eca5a98271dfcb66f5110d26c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ec7314dc68558163171",
+    "url": "/BrendeaDaniel/static/css/main.2f723ce2.chunk.css"
+  },
+  {
+    "revision": "3ec7314dc68558163171",
+    "url": "/BrendeaDaniel/static/js/main.7d0523f8.chunk.js"
+  },
+  {
+    "revision": "623492eefdc70d07d17d",
+    "url": "/BrendeaDaniel/static/js/runtime~main.043e3383.js"
+  },
+  {
+    "revision": "5bc574cc018ae2c67130",
+    "url": "/BrendeaDaniel/static/js/2.75d26613.chunk.js"
+  },
+  {
+    "revision": "594a3e89b2c08a192f7de1c2219aa270",
+    "url": "/BrendeaDaniel/static/media/indeximagemod.594a3e89.jpeg"
+  },
+  {
+    "revision": "d7e11aa753df3ada37afba80de6cd093",
+    "url": "/BrendeaDaniel/static/media/daniel.d7e11aa7.png"
+  },
+  {
+    "revision": "ab83a852da1a6add9bb59a8ef187a048",
+    "url": "/BrendeaDaniel/static/media/solo.ab83a852.png"
+  },
+  {
+    "revision": "97a3b1edb3207dce79902b8879cd4bf5",
+    "url": "/BrendeaDaniel/static/media/ecommerce.97a3b1ed.png"
+  },
+  {
+    "revision": "ca08eba2116917ecaae9c66f04c2fac5",
+    "url": "/BrendeaDaniel/static/media/whackAMole.ca08eba2.png"
+  },
+  {
+    "revision": "f99e8612cd3b67028fd55477887a87c6",
+    "url": "/BrendeaDaniel/static/media/weather-app.f99e8612.png"
+  },
+  {
+    "revision": "db76764bbb0efd6814f61d903a91af67",
+    "url": "/BrendeaDaniel/static/media/burger.db76764b.png"
+  },
+  {
+    "revision": "f2a4eed7ef23fcc534c95be8f5f8e273",
+    "url": "/BrendeaDaniel/static/media/memories.f2a4eed7.png"
+  },
+  {
+    "revision": "1077898f8ebec492ad22d66d2db2f629",
+    "url": "/BrendeaDaniel/static/media/CV-EN-Brendea-Daniel2.1077898f.pdf"
+  },
+  {
+    "revision": "4c44638d421a76f5e758e105c20ea21a",
+    "url": "/BrendeaDaniel/index.html"
+  }
+];
