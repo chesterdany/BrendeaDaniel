cb72287ab190847555b02a9778f4171b552c4797
Brendea Daniel <chesterdany22@yahoo.com>
2021-03-03 14:20:07 +0200

    new project added

--- src/UI/ProjectPage/ProjectPage.js
+++ src/UI/ProjectPage/ProjectPage.js
@@ -1,23 +1,19 @@
-import React from "react";
-import Navbar from "../NavBar/Navbar";
-import classes from "./ProjectPage.module.css";
-import Project from "./Project";
-import todoList from "../../assests/images/todolist.png";
-import solo from "../../assests/images/solo.png";
-import whack from "../../assests/images/whackAMole.png";
-import weather from "../../assests/images/weather-app.png";
-import burger from "../../assests/images/burger.png";
-import indeximage from "../../assests/images/indeximagemod.jpeg";
-import memories from "../../assests/images/memories.png";
+import React from 'react';
+import Navbar from '../NavBar/Navbar';
+import classes from './ProjectPage.module.css';
+import Project from './Project';
+import solo from '../../assests/images/solo.png';
+import ecommerce from '../../assests/images/ecommerce.png';
+import whack from '../../assests/images/whackAMole.png';
+import weather from '../../assests/images/weather-app.png';
+import burger from '../../assests/images/burger.png';
+import indeximage from '../../assests/images/indeximagemod.jpeg';
+import memories from '../../assests/images/memories.png';
 
 const projectPage = () => {
   return (
     <React.Fragment>
-      <img
-        className={classes.IndexImage}
-        src={indeximage}
-        alt="backgroundImage"
-      />
+      <img className={classes.IndexImage} src={indeximage} alt="backgroundImage" />
       <Navbar />
 
       <div className={classes.ProjectPage}>
@@ -25,15 +21,16 @@ const projectPage = () => {
         <hr className={classes.Hr} />
         <div className={classes.Gallery}>
           <Project
+            src={ecommerce}
+            apphref="https://test-ecommerce22.netlify.app/"
+            sourcehref="https://github.com/chesterdany/e-commerce-"
+          />
+          <Project
             src={memories}
             apphref="https://chesterdany.github.io/memories-app/"
             sourcehref="https://github.com/chesterdany/memories-app"
           />
-          <Project
-            src={whack}
-            apphref="https://chesterdany.github.io/Whack-a-mole/"
-            sourcehref="https://github.com/chesterdany/Whack-a-mole"
-          />
+
           <Project
             src={weather}
             apphref="https://weather-pwaapp.netlify.app/"
@@ -44,11 +41,10 @@ const projectPage = () => {
             apphref="https://burger-app-fd68d.web.app/"
             sourcehref="https://github.com/chesterdany/burger"
           />
-
           <Project
-            src={todoList}
-            apphref="https://chesterdany.github.io/todo-list/"
-            sourcehref="https://github.com/chesterdany/todo-list"
+            src={whack}
+            apphref="https://chesterdany.github.io/Whack-a-mole/"
+            sourcehref="https://github.com/chesterdany/Whack-a-mole"
           />
           <Project
             src={solo}
