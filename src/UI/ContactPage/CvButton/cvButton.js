0c02bf63a7784d292be884a2aa8fb72aac3d8c3a
Brendea Daniel <chesterdany22@yahoo.com>
2021-03-12 22:07:57 +0200

    cv link changed

--- src/UI/ContactPage/CvButton/cvButton.js
+++ src/UI/ContactPage/CvButton/cvButton.js
@@ -1,15 +1,15 @@
-import React from "react";
-import cv from "../../../assests/cv/CV-EN-Brendea-Daniel2.pdf";
-import classes from "./cvButton.module.css";
+import React from 'react';
+import cv from '../../../assests/cv/CV-EN-Brendea-Daniel2.pdf';
+import classes from './cvButton.module.css';
 
 const cvButton = () => {
-  return (
-    <div className={classes.cvdivlink}>
-      <a href={cv} target="_blank" rel="noopener noreferrer">
-        - View CV -
-      </a>
-    </div>
-  );
+    return (
+        <div className={classes.cvdivlink}>
+            <a href={cv} target="_blank" rel="noopener noreferrer">
+                - View CV -
+            </a>
+        </div>
+    );
 };
 
 export default cvButton;
