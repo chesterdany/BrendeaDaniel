2ca49da67e0a39a0a578781908a6972187acd491
Brendea Daniel <chesterdany22@yahoo.com>
2021-11-15 21:38:13 +0200

    4k media query added

--- src/UI/ContactPage/ContactPage.js
+++ src/UI/ContactPage/ContactPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../NavBar/Navbar";
 import classes from "./ContactPage.module.css";
-import { FaGithub, FaFacebook, FaLinkedin, FaAt, FaSkype, FaMobileAlt } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaAt, FaSkype, FaMobileAlt } from "react-icons/fa";
 // import CvButton from "./CvButton/cvButton";
 import Plink from "../HomePage/Plink/pLink";
 import ContactForm from "./ContactForm/ContactForm";
@@ -38,9 +38,6 @@ const contactPage = () => {
               <Plink href="https://www.linkedin.com/in/brendea-daniel-a261a81a2/">
                 <FaLinkedin className={classes.link} />
               </Plink>
-              {/* <Plink href="https://www.facebook.com/chester.dani.1/">
-                <FaFacebook className={classes.link} />
-              </Plink> */}
             </div>
             {/* <CvButton className={classes.btn} /> */}
           </div>
